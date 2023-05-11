1b4c09016fa982a845c93b53ad8557a6200f10ef
Anete <anetempereira@gmail.com>
2023-05-11 23:59:30 +0100

    started implementing the game logic

--- assign2/src/Game.java
+++ assign2/src/Game.java
@@ -1,3 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+// TODO: APAGAR
+import java.util.Scanner;
+
+
 public class Game {
+
+        Random random = new Random();
+        
+
+
+        // Choose a random word from the list of words
+        public String chooseWord(List<String> words, Random random) { 
+            int wordsLength = words.size();
+            int randomIndex =  random.nextInt(wordsLength);
+
+            return words.get(randomIndex);
+        }
+
+
+        // Convert all lettters in a client word guess to capital letters 
+        public String convertToCapitalLetters (String word) {
+            return word.toUpperCase();
+        }
+
+
+        public String shuffleWord(String word, Random random) {
+            // Convert the string into an array of chars
+            char letters[] = word.toCharArray();
+
+            // Using Fisher-Yates algorithm to shuffle the word
+            for (int i=0; i < letters.length; i++){
+                int j = random.nextInt(letters.length);
+                char temp = letters[i];
+                letters[i] = letters[j];
+                letters[j] = temp;
+            }
+
+            String resultWord = new String(letters); 
+            
+            // TODO: For cases where the resulting word is equal to ther one given, not working  
+            if (resultWord == word){
+                shuffleWord(word, new Random());
+            }
+
+            return resultWord;
+        }
+
+        public MessageType compareWords(String guess, String correctWord){
+            MessageType response;
+            if (guess == correctWord){
+                response = MessageType.CORRECT_GUESS;
+            } else {
+                response = MessageType.INCORRECT_GUESS;
+            }
+            return response;
+        }
+
+        public static void main(String[] args) {
+            //testPlayGame();
+        }
 
 }
